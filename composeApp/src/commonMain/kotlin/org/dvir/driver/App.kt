02b935c61fc8f9eaff23c630875b9720b2a534c6
dvir <dvirm.biton@gmail.com>
2024-12-10 19:32:58 +0200

    Fixed Koin setup on IOS

--- composeApp/src/commonMain/kotlin/org/dvir/driver/App.kt
+++ composeApp/src/commonMain/kotlin/org/dvir/driver/App.kt
@@ -2,18 +2,13 @@ package org.dvir.driver
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import org.dvir.driver.di.appModule
 import org.dvir.driver.presentation.home.HomeScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.koinInject
-import org.koin.core.context.startKoin
 
 @Composable
 @Preview
 fun App() {
-    startKoin {
-        modules(listOf(appModule))
-    }
     MaterialTheme {
         HomeScreen(koinInject())
     }
--- composeApp/src/iosMain/kotlin/org/dvir/driver/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/dvir/driver/MainViewController.kt
@@ -1,5 +1,21 @@
 package org.dvir.driver
 
 import androidx.compose.ui.window.ComposeUIViewController
+import org.dvir.driver.di.appModule
+import org.koin.core.context.startKoin
 
-fun MainViewController() = ComposeUIViewController { App() }
+var wasKoinInitialized = false
+
+fun MainViewController() = ComposeUIViewController {
+    initKoin()
+    App()
+}
+
+fun initKoin() {
+    if (!wasKoinInitialized) {
+        startKoin {
+            modules(appModule)
+        }
+        wasKoinInitialized = true
+    }
+}
